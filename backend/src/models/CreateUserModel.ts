496d5b90a20c28f713365346002b62dd346d0c8c
João Vitor Lima <jvolima2004@gmail.com>
2022-06-22 10:26:20 -0300

    validate errors

--- backend/src/models/CreateUserModel.ts
+++ backend/src/models/CreateUserModel.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../errors/AppError';
 import { prisma } from '../prisma';
 
 export interface User {
@@ -12,19 +13,37 @@ export interface User {
 
 export class CreateUserModel {
   async newUser(data: User) {
-    const userAlreadyExists = await prisma.client.findUnique({
+    const userAlreadyExistsWithEmail = await prisma.client.findUnique({
       where: {
         email: data.email
       }
     });
 
-    if(userAlreadyExists) {
-      throw new Error();
+    const userAlreadyExistWithCpf = await prisma.client.findFirst({
+      where:{
+        cpf: data.cpf
+      }
+    })
+
+    if(userAlreadyExistsWithEmail) {
+      throw new AppError('User with this email already exists');
+    }
+
+    if(userAlreadyExistWithCpf) {
+      throw new AppError('User with this cpf already exists');
+    }
+
+    if(data.name.trim() === "") {                                 
+      throw new AppError('Invalid name');
+    }
+
+    if(data.password.trim().length < 6 || data.password.trim().length > 30) {
+      throw new AppError('Password must be between 6 and 30 characters');
     }
 
     const user = await prisma.client.create({
       data: {
-        name: data.name,
+        name: data.name.trim(),
         email: data.email,
         password: data.password,
         phone: data.phone,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import { AppError } from './errors/AppError';
 import { routes } from './routes';
 
 const app = express();
@@ -6,5 +8,20 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message
+      })
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`
+    })
+  }
+)
 
 app.listen(4000, () => console.log('Server is running'));
